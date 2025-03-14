e4df68643d88ba9a880d6ca571ffb0064e582a3b
zakaria <zaki.guellouch@gmail.com>
2025-03-14 10:27:53 +0000

    life is good

--- index.js
+++ index.js
@@ -149,9 +149,9 @@ app.get('/xml', (req, res) => {
     res.send('<note><body>This is XML</body></note>');
 });
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+// const PORT = process.env.PORT || 3001
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
 
 module.exports = app;
