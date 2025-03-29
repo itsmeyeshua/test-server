9358e4f6bb1f9e7d1fab12ebc75d1c22eb72013e
zakaria <zaki.guellouch@gmail.com>
2025-03-29 18:32:18 +0000

    just removed cors because i'm not using it anymore

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express")
 const path = require('path');
 const app = express();
 const morgan = require("morgan")
-const cors = require('cors')
-app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 // app.use(morgan('tiny')); //Morgan is a logging middleware for Express.
